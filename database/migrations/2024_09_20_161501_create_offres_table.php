50613910fc47955e88f35740a56759cfb94cebc6
fatima-harun <faabintu@gmail.com>
2024-09-21 20:21:02 +0000

    ajout du seeder

---
+++ database/migrations/2024_09_20_161501_create_offres_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offres', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->string('lieu');
+            $table->string('salaire');
+            $table->string('horaire');
+            $table->date('datedebut');
+            $table->text('profil');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offres');
+    }
+};
---
+++ database/seeders/ServiceSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Service;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $services = [
+           [
+                'libelle' =>"Chauffeur"
+           ],
+
+           [
+            'libelle' =>"Aide ménagère"
+           ],
+
+           [
+            'libelle' =>"Cuisinier(e)"
+           ],
+
+           [
+            'libelle' =>"Gardien(ne)"
+           ],
+
+           [
+            'libelle' =>"Nounou"
+           ],
+        ];
+        foreach($services as $service){
+            Service::create($services);
+        }
+    }
+}

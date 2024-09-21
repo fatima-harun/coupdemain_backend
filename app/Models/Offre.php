d85f4703e96c4bf33d2e1293b28df66008294f3a
fatima-harun <faabintu@gmail.com>
2024-09-21 22:49:38 +0000

    ajout des cles etrangeres

--- app/Models/Offre.php
+++ app/Models/Offre.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Service;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Offre extends Model
 {
     use HasFactory;
+
+    public function services()
+    {
+        return $this->belongsToMany(Service::class);
+    }
 }
--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Offre;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Service extends Model
 {
     use HasFactory;
+   
+
+    public function offres()
+    {
+        return $this->belongsToMany(Offre::class);
+    }
 }
